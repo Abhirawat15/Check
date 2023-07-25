d032bf2079913d22fad40e6c02e548e04f41ab75
Abhirawat15 <abhishekrawatvs158@gmail.com>
2023-07-25 18:17:50 +0530

    Adding removeZeroAfterDot method

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -56,6 +56,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun removeZeroAfterDot(result:String) :String{
+        var value=result
+        if(result.contains(".0"))
+            value=result.substring(0,result.length-2)
+
+        return value
+    }
+
     fun onEqual(view:View){
         if(lastNumeric){
             var tvValue=tvInput?.text.toString()
@@ -76,7 +84,40 @@ class MainActivity : AppCompatActivity() {
                         one=prefix+one
                     }
 
-                    tvInput?.text=(one.toDouble()-two.toDouble()).toString()
+                    tvInput?.text=removeZeroAfterDot((one.toDouble()-two.toDouble()).toString())
+                }else if(tvValue.contains("+")){
+                    val splitValue=tvValue.split("+")
+
+                    var one=splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+
+                    tvInput?.text=removeZeroAfterDot((one.toDouble()+two.toDouble()).toString())
+                }else if(tvValue.contains("*")){
+                    val splitValue=tvValue.split("*")
+
+                    var one=splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+
+                    tvInput?.text=removeZeroAfterDot((one.toDouble()*two.toDouble()).toString())
+                }else if(tvValue.contains("/")){
+                    val splitValue=tvValue.split("/")
+
+                    var one=splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+
+                    tvInput?.text=removeZeroAfterDot((one.toDouble()/two.toDouble()).toString())
                 }
             }catch (e:ArithmeticException){
                 e.printStackTrace()
